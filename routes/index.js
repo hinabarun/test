007d35211c9122f67624ed994f1e4d9ef4a03147
unknown <hinabarun@gmail.com>
2020-01-27 17:15:59 +0530

    first commit

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+//  res.render('index', { title: 'Azure' });
+
+var model = {
+    tite : 'Azure',
+    message : process.env.message || "This is Nabarun"
+};
+res.render ('index',model);
+
 });
 
 module.exports = router;
